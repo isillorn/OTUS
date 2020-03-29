f7ef8478f0d73de4990d9921e7aed9cd97448e83
isillorn <kholodoff@gmail.com>
2020-03-29 14:15:47 +0800

    1

---
+++ lesson1-now.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+
+	if time, err := ntp.Time("ru.pool.ntp.org"); err != nil {
+		fmt.Fprint(os.Stderr, err, " (error code = 1)\n")
+
+	} else {
+		fmt.Printf("%s", time)
+	}
+}
